419c0b685907133ab189f162c4a3760264efde1c
Jeremy Diaz <jedi4302@colorado.edu>
2019-06-07 15:29:59 +0000

    Adding script to download noaa/storm-events tor paths

---
+++ download_path_data.sh
@@ -0,0 +1,11 @@
+
+
+
+# Download NOAA-provided zipped data
+wget https://www.spc.noaa.gov/gis/svrgis/zipped/1950-2017-torn-aspath.zip
+
+
+# Unzip the data
+unzip 1950-2017-torn-aspath.zip -d 1950-2017-tornado-aspath
+
+
