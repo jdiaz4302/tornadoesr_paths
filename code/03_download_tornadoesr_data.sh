40fa021644f8df82bf2946010c70090e8f342c5c
Jeremy Diaz <jedi4302@colorado.edu>
2019-06-20 17:58:18 +0000

    Updating workflow to use raw Storm Events data rather than tornadoesr

---
+++ code/03_download_tornadoesr_data.sh
@@ -0,0 +1,10 @@
+
+
+
+# Download the 2017 Storm Events data
+wget ftp://ftp.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/StormEvents_details-ftp_v1.0_d2017_c20190516.csv.gz -P data/
+
+
+# Unzip it
+gunzip data/StormEvents_details-ftp_v1.0_d2017_c20190516.csv.gz
+
